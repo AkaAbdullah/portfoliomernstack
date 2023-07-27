a42b5571338b629ddb7270ae7b4d936f2c789055
Abdullah Hussain <akaabdullah00@gmail.com>
2023-07-28 01:04:56 +0500

    folder structure done

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,10 @@
-console.log("lori");
+const express = require("express");
+const dotenv = require("dotenv").config();
+const port = process.env.PORT || 8000;
+const dbConnect = require("./config/dbConn");
+
+dbConnect();
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.listen(port, () => console.log(`server started on port ${port}`));
---
+++ frontend/src/pages/AdminPage.jsx
@@ -0,0 +1,9 @@
+export const AdminPage = () => {
+  return (
+    <>
+      <section>
+        <p>Admin Page</p>
+      </section>
+    </>
+  );
+};
